30b7e40fae07d329fe37c3ba7b19747f1e816a84
vidya414 <47877632+vidya414@users.noreply.github.com>
2019-03-18 20:02:10 +0530

    scinterest.java
    to calculate simple interest and compound interest

---
+++ scinterest.java
@@ -0,0 +1,17 @@
+import java.util .*
+class scinterest
+{
+    double p,r,t,s,c;//p=principle amount,r=rate of interest,t=time,s=simple interest,c-=compound interest
+    Scanner s=new Scanner (System.in);
+    System.out.println("Enter the amount:");
+    p=s.nextDouble();
+    //Enter the No.of years:
+    t=s.nextDouble();
+    //Enter the Rate of  interest
+    r=s.nextDouble();
+    s=((p * t * r)/100);
+    c=p * Math.pow(1.0+r/100.0,t) - p;
+    //Simple Interest=s
+    //Compound Interest=c
+   }
+}
